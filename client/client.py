9b37b86df9493a02c41e2fad624808a47944df43
zizhazhu <zizhazhu@gmail.com>
2018-02-27 13:22:46 +0800

    use larger gap

--- client/client.py
+++ client/client.py
@@ -2,15 +2,17 @@ import socket
 import time
 import random
 
+name = 'test'
+beat_gap = 3600
+
 host = '127.0.0.1'
 port = 12345
-name = 'test'
 
 udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
     data = name.encode()
     udp_client.sendto(data, (host, port))
-    time.sleep(0 + random.randint(10, 100))
+    time.sleep(beat_gap + random.randint(10, 100))
 
 udp_client.close()
